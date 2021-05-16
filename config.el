d8365dd4f933615fc868883a8d6a6a39fb73634a
James Stone <jamesmstone@users.noreply.github.com>
2021-05-16 16:58:53 +1000

    inline email config into main config

--- config.el
+++ config.el
@@ -1,158 +1,75 @@
-(map! :leader
-      :desc "List bookmarks"
-      "b L" #'list-bookmarks
-      :leader
-      :desc "Save current bookmarks to bookmark file"
-      "b w" #'bookmark-save)
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/mu4e")
+;;(require 'smtpmail)
+(setq user-mail-address "jamesmstone@hotmail.com"
+      user-full-name  "James Stone"
+      ;; I have my mbsyncrc in a different folder on my system, to keep it separate from the
+      ;; mbsyncrc available publicly in my dotfiles. You MUST edit the following line.
+      ;; Be sure that the following command is: "mbsync -c ~/.config/mu4e/mbsyncrc -a"
+      mu4e-get-mail-command "mbsync -a"
+      mu4e-update-interval  300
+      mu4e-main-buffer-hide-personal-addresses t
+      message-send-mail-function 'smtpmail-send-it
+      starttls-use-gnutls t
+      smtpmail-starttls-credentials '(("smtp.1and1.com" 587 nil nil))
+      mu4e-sent-folder "/outlook/Sent"
+      mu4e-drafts-folder "/outlook/Drafts"
+      mu4e-trash-folder "/outlook/Trash"
+      mu4e-maildir-shortcuts
+      '(("/outlook/inbox"       . ?i)
+        ("/outlook/Sent"        . ?s)
+        ("/outlook/Drafts"      . ?d)
+        ("/outlook/Deleted"     . ?t)))
 
-(setq centaur-tabs-set-bar 'over
-      centaur-tabs-set-icons t
-      centaur-tabs-gray-out-icons 'buffer
-      centaur-tabs-height 24
-      centaur-tabs-set-modified-marker t
-      centaur-tabs-style "bar"
-      centaur-tabs-modified-marker "•")
-(map! :leader
-      :desc "Toggle tabs globally"
-      "t c" #'centaur-tabs-mode
-      :leader
-      :desc "Toggle tabs local display"
-      "t C" #'centaur-tabs-local-mode)
-(evil-define-key 'normal centaur-tabs-mode-map (kbd "g <right>") 'centaur-tabs-forward        ; default Doom binding is 'g t'
-                                               (kbd "g <left>")  'centaur-tabs-backward       ; default Doom binding is 'g T'
-                                               (kbd "g <down>")  'centaur-tabs-forward-group
-                                               (kbd "g <up>")    'centaur-tabs-backward-group)
+(defvar my-mu4e-account-alist
+  '(("exchange"
+     (mu4e-sent-folder "/exchange/Sent")
+     (mu4e-drafts-folder "/exchange/Draft")
+     (mu4e-trash-folder "/exchange/Trash")
+     (mu4e-compose-signature
+       (concat
+         "James Stone\n"
+         "james@renewabeenergyhub.com.au\n"))
+     (user-mail-address "james.stone@traditionasia.com")
+     (smtpmail-default-smtp-server "smtp.domain.com")
+     (smtpmail-smtp-server "smtp.domain.com")
+     (smtpmail-smtp-user "acc1@domain.com")
+     (smtpmail-stream-type starttls)
+     (smtpmail-smtp-service 587))
+    ("gmail"
+     (mu4e-sent-folder "/gmail/Sent")
+     (mu4e-drafts-folder "/gmail/Drafts")
+     (mu4e-trash-folder "/gmail/Trash")
+     (mu4e-compose-signature
+       (concat
+         "John Boy\n"
+         "acc3@domain.com\n"))
+     (user-mail-address "jamesmstone711@gmail.com")
+     (smtpmail-default-smtp-server "smtp.domain.com")
+     (smtpmail-smtp-server "smtp.domain.com")
+     (smtpmail-smtp-user "acc3@domain.com")
+     (smtpmail-stream-type starttls)
+     (smtpmail-smtp-service 587))))
 
-(map! :leader
-      :desc "Dired"
-      "d d" #'dired
-      :leader
-      :desc "Dired jump to current"
-      "d j" #'dired-jump
-      (:after dired
-        (:map dired-mode-map
-         :leader
-         :desc "Peep-dired image previews"
-         "d p" #'peep-dired
-         :leader
-         :desc "Dired view file"
-         "d v" #'dired-view-file)))
-;; Make 'h' and 'l' go back and forward in dired. Much faster to navigate the directory structure!
-(evil-define-key 'normal dired-mode-map
-  (kbd "h") 'dired-up-directory
-  (kbd "l") 'dired-open-file) ; use dired-find-file instead if not using dired-open package
-;; If peep-dired is enabled, you will get image previews as you go up/down with 'j' and 'k'
-(evil-define-key 'normal peep-dired-mode-map
-  (kbd "j") 'peep-dired-next-file
-  (kbd "k") 'peep-dired-prev-file)
-(add-hook 'peep-dired-hook 'evil-normalize-keymaps)
-;; Get file icons in dired
-(add-hook 'dired-mode-hook 'all-the-icons-dired-mode)
-;; With dired-open plugin, you can launch external programs for certain extensions
-;; For example, I set all .png files to open in 'sxiv' and all .mp4 files to open in 'mpv'
-(setq dired-open-extensions '(("gif" . "sxiv")
-                              ("jpg" . "sxiv")
-                              ("png" . "sxiv")
-                              ("mkv" . "mpv")
-                              ("mp4" . "mpv")))
+(defun my-mu4e-set-account ()
+  "Set the account for composing a message."
+  (let* ((account
+          (if mu4e-compose-parent-message
+              (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+                (string-match "/\\(.*?\\)/" maildir)
+                (match-string 1 maildir))
+            (completing-read (format "Compose with account: (%s) "
+                                     (mapconcat #'(lambda (var) (car var))
+                                                my-mu4e-account-alist "/"))
+                             (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+                             nil t nil nil (caar my-mu4e-account-alist))))
+         (account-vars (cdr (assoc account my-mu4e-account-alist))))
+    (if account-vars
+        (mapc #'(lambda (var)
+                  (set (car var) (cadr var)))
+              account-vars)
+      (error "No email account found"))))
 
-(setq doom-theme 'doom-solarized-light)
-(map! :leader
-      :desc "Load new theme"
-      "h t" #'counsel-load-theme)
-
-(require 'elfeed-goodies)
-(elfeed-goodies/setup)
-(setq elfeed-goodies/entry-pane-size 0.5)
-(add-hook 'elfeed-show-mode-hook 'visual-line-mode)
-(evil-define-key 'normal elfeed-show-mode-map
-  (kbd "J") 'elfeed-goodies/split-show-next
-  (kbd "K") 'elfeed-goodies/split-show-prev)
-(evil-define-key 'normal elfeed-search-mode-map
-  (kbd "J") 'elfeed-goodies/split-show-next
-  (kbd "K") 'elfeed-goodies/split-show-prev)
-(setq elfeed-feeds (quote
-                    (("https://www.reddit.com/r/linux.rss" reddit linux)
-                     ("https://www.reddit.com/r/commandline.rss" reddit linux)
-                     ("https://www.reddit.com/r/emacs.rss" reddit linux)
-                     ("https://www.gamingonlinux.com/article_rss.php" gaming linux)
-                     ("https://hackaday.com/blog/feed/" hackaday linux)
-                     ("https://opensource.com/feed" opensource linux)
-                     ("https://linux.softpedia.com/backend.xml" softpedia linux)
-                     ("https://itsfoss.com/feed/" itsfoss linux)
-                     ("https://www.zdnet.com/topic/linux/rss.xml" zdnet linux)
-                     ("https://www.phoronix.com/rss.php" phoronix linux)
-                     ("http://feeds.feedburner.com/d0od" omgubuntu linux)
-                     ("https://www.computerworld.com/index.rss" computerworld linux)
-                     ("https://www.networkworld.com/category/linux/index.rss" networkworld linux)
-                     ("https://www.techrepublic.com/rssfeeds/topic/open-source/" techrepublic linux)
-                     ("https://betanews.com/feed" betanews linux)
-                     ("http://lxer.com/module/newswire/headlines.rss" lxer linux)
-                     ("https://distrowatch.com/news/dwd.xml" distrowatch linux))))
-
-(map! :leader
-      :desc "Evaluate elisp in buffer"
-      "e b" #'eval-buffer
-      :leader
-      :desc "Evaluate defun"
-      "e d" #'eval-defun
-      :leader
-      :desc "Evaluate elisp expression"
-      "e e" #'eval-expression
-      :leader
-      :desc "Evaluate last sexpression"
-      "e l" #'eval-last-sexp
-      :leader
-      :desc "Evaluate elisp in region"
-      "e r" #'eval-region)
-
-(setq browse-url-browser-function 'eww-browse-url)
-(map! :leader
-      :desc "Eww web browser"
-      "e w" #'eww
-      :leader
-      :desc "Eww reload page"
-      "e R" #'eww-reload
-      :leader
-      :desc "Search web for text between BEG/END"
-      "s w" #'eww-search-words)
-
-(setq doom-font (font-spec :family "Mononoki Nerd Font" :size 15)
-      doom-variable-pitch-font (font-spec :family "Ubuntu" :size 15)
-      doom-big-font (font-spec :family "Mononoki Nerd Font" :size 24))
-(after! doom-themes
-  (setq doom-themes-enable-bold t
-        doom-themes-enable-italic t))
-(custom-set-faces!
-  '(font-lock-comment-face :slant italic)
-  '(font-lock-keyword-face :slant italic))
-
-(setq display-line-numbers-type t)
-(map! :leader
-      :desc "Comment or uncomment lines"
-      "TAB TAB" #'comment-line
-      :leader
-      :desc "Toggle line numbers"
-      "t l" #'doom/toggle-line-numbers
-      :leader
-      :desc "Toggle line highlight in frame"
-      "t h" #'hl-line-mode
-      :leader
-      :desc "Toggle line highlight globally"
-      "t H" #'global-hl-line-mode
-      :leader
-      :desc "Toggle truncate lines"
-      "t t" #'toggle-truncate-lines)
-
-(xterm-mouse-mode 1)
-(global-set-key [mouse-4] (lambda ()
-                              (interactive)
-                              (scroll-down 1)))
-(global-set-key [mouse-5] (lambda ()
-                              (interactive)
-                              (scroll-up 1)))
-
-(load "~/.doom.d/email")
+(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
 
 (map! :leader
       :desc "Edit agenda file"
